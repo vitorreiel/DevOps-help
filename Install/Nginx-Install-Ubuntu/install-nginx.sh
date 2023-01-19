b1c8cfbcd43f611ab7cc64875a7de2142a5a7408
Vitor Reiel <52376553+vitorreiel@users.noreply.github.com>
2023-01-19 19:27:52 -0300

    att

--- Install/Nginx-Install-Ubuntu/install-nginx.sh
+++ Install/Nginx-Install-Ubuntu/install-nginx.sh
@@ -13,7 +13,7 @@ sudo apt install nginx -y > /dev/null 2>&1
 sudo systemctl start nginx
 sudo systemctl enable nginx
 while [ $i -ge $l ]; do
-	sudo touch /var/www/${array[i]}
+	sudo mkdir /var/www/${array[i]}
 	echo "server {
 		listen 80;
 		listen [::]:80;
@@ -29,6 +29,7 @@ while [ $i -ge $l ]; do
 	}" | tee -a ${array[i]} > /dev/null 2>&1
 	sudo mv ${array[i]} /etc/nginx/sites-available/
 	sudo ln -s /etc/nginx/sites-available/${array[i]} /etc/nginx/sites-enabled/
+	sudo rm /etc/nginx/sites-enabled/sites-available
 	i=$((i-1))
 done
 sudo unlink /etc/nginx/sites-enabled/default
