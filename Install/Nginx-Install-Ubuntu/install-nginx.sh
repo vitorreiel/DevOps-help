abdb4a4968378bd82b52b0d7759d63d642cbb52c
Vitor Reiel <52376553+vitorreiel@users.noreply.github.com>
2023-01-19 19:56:01 -0300

    Update install-nginx.sh

--- Install/Nginx-Install-Ubuntu/install-nginx.sh
+++ Install/Nginx-Install-Ubuntu/install-nginx.sh
@@ -10,26 +10,29 @@ while [ $i -le $num ]; do
 done
 sudo apt update > /dev/null 2>&1
 sudo apt install nginx -y > /dev/null 2>&1
+echo -e "\033[1;32m- [RUNNING Nginx] ---------------------- \033[0m       --> 1;32 "
 sudo systemctl start nginx
+echo -e "\033[1;32m- [ENABLE Nginx.service] ---------------------- \033[0m       --> 1;32 "
 sudo systemctl enable nginx
 while [ $i -ge $l ]; do
 	sudo mkdir /var/www/${array[i]} > /dev/null 2>&1
-	echo "server {
-		listen 80;
-		listen [::]:80;
+echo "server {
+	listen 80;
+	listen [::]:80;
 
-		root /var/www/${array[i]};
-		index index.html index.htm index.nginx-debian.html;
+	root /var/www/${array[i]};
+	index index.html index.htm index.nginx-debian.html;
 
-		server_name ${array[i]}.com www.${array[i]}.com;
+	server_name ${array[i]}.com www.${array[i]}.com;
 
-		location / {
-			try_files $uri $uri/ =404;
-		}
-	}" | tee -a ${array[i]} > /dev/null 2>&1
+	location / {
+		try_files $uri $uri/ =404;
+	}
+}" | tee -a ${array[i]} > /dev/null 2>&1
 	sudo mv ${array[i]} /etc/nginx/sites-available/ > /dev/null 2>&1
 	sudo ln -s /etc/nginx/sites-available/${array[i]} /etc/nginx/sites-enabled/ > /dev/null 2>&1
 	i=$((i-1))
 done
 sudo rm /etc/nginx/sites-enabled/sites-available > /dev/null 2>&1
 sudo unlink /etc/nginx/sites-enabled/default > /dev/null 2>&1
+echo -e "\033[1;32m- [Domains Successfully Created] ---------------------- \033[0m       --> 1;32 "
