7b31becf5c82b2255af0114c3f7dafbce298b2f0
Vitor Reiel <52376553+vitorreiel@users.noreply.github.com>
2023-01-20 17:41:31 -0300

    att

--- Install/Sonarqube-Install-Ubuntu/install-sonarqube.sh
+++ Install/Sonarqube-Install-Ubuntu/install-sonarqube.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 # Install Sonarqube, Sonar-scanner, JDK, Unzip, Wget
-echo -e "\033[1;32m- [ INSTALLING Java ] ------------------------------------------------------------------------------ \033[0m"
-sudo apt install wget unzip default-jdk -y > /dev/null 2>&1
-echo -e "\033[1;32m- [ INSTALLING Sonarqube ] ------------------------------------------------------------------------- \033[0m"
-sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.8.0.63668.zip > /dev/null 2>&1
+echo -e "\033[1;32m- [ INSTALLING Java ] ------------------------------------------------------------------------------ \033[0m"sudo apt install wget unzip default-jdk -y > /dev/null 2>&1
+echo -e "\033[1;32m- [ INSTALLING Sonarqube ] ------------------------------------------------------------------------- \033[0m"sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.8.0.63668.zip > /dev/null 2>&1
 sudo unzip sonarqube-9.8.0.63668.zip -d /opt/ > /dev/null 2>&1
 sudo mv /opt/sonarqube-9.8.0.63668 /opt/sonarqube
-echo -e "\033[1;32m- [ CREATE User 'sonar' ] ------------------------------------------------------------------------------ \033[0m"
+echo -e "\033[1;32m- [ CREATE User 'sonar' ] -------------------------------------------------------------------------- \033[0m"
 sudo useradd sonar
 sudo chown -R sonar:sonar /opt/sonarqube
-echo -e "\033[1;32m- [ CREATE sonar.service ] ------------------------------------------------------------------------------ \033[0m"
+echo -e "\033[1;32m- [ CREATE sonar.service ] ------------------------------------------------------------------------- \033[0m"
+DIR=/etc/systemd/system/sonar.service
+if [ -e "$DIR" ]; then
+sudo systemctl start sonar.service
+else
 echo "[Unit]
 Description=Sonarqube service
 After=syslog.target network.target
@@ -24,20 +26,20 @@ Group=sonar
 Restart=always
 [Install]
 WantedBy=multi-user.target" | tee -a sonar.service > /dev/null 2>&1
-sudo mv sonar.service /etc/systemd/system/
-echo -e "\033[1;32m- [ STARTING sonar.service ] ------------------------------------------------------------------------------ \033[0m"
 sudo systemctl start sonar.service
+fi
+echo -e "\033[1;32m- [ STARTING sonar.service ] ----------------------------------------------------------------------- \033[0m"
 
 # Install Sonar Scanner
-echo -e "\033[1;32m- [ INSTALLING Sonar-scanner ] ------------------------------------------------------------------------------ \033[0m"
+echo -e "\033[1;32m- [ INSTALLING Sonar-scanner ] --------------------------------------------------------------------- \033[0m"
 sudo wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.7.0.2747-linux.zip > /dev/null 2>&1
 sudo unzip sonar-scanner-cli-4.7.0.2747-linux.zip -d /opt/ > /dev/null 2>&1
 sudo mv /opt/sonar-scanner-4.7.0.2747-linux /opt/sonar-scanner
 sudo chown -R sonar:sonar /opt/sonar-scanner
-echo -e "\033[1;32m- [ CREATE variable global 'sonar-scanner' ] ------------------------------------------------------------------------------ \033[0m"
+echo -e "\033[1;32m- [ CREATE variable global 'sonar-scanner' ] ---------------------------------------------------------------- \033[0m"
 PATH=$PATH:/opt/sonar-scanner/bin
 sudo rm sonar*
-echo -e "\033[1;32m- [ Successfully Install Sonarqube and Sonar-Scanner ] ---------------------------------------------------------------- \033[0m"
+echo -e "\033[1;32m- [ Successfully Install Sonarqube and Sonar-Scanner ] ------------------------------------------------------ \033[0m"
 # echo /etc/environment
 # If S.O (CentOS or RedHat)
 #echo 'export PATH=$PATH:/opt/sonar-scanner/bin' | sudo tee -a /etc/profile
