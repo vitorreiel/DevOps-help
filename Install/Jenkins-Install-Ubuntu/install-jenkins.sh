e57596418ee3c7d898ca9dac1beef835ecb8fb54
Vitor Reiel <vitorreiel@hotmail.com>
2023-01-20 20:37:18 -0300

    att

--- Install/Jenkins-Install-Ubuntu/install-jenkins.sh
+++ Install/Jenkins-Install-Ubuntu/install-jenkins.sh
@@ -13,7 +13,7 @@ sudo echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
 sudo apt update > /dev/null 2>&1
 echo -e "\033[1;32m- [ INSTALLING Jenkins ] --------------------------------------------------------------------------- \033[0m"
 sudo apt install jenkins -y > /dev/null 2>&1
-echo -e "\033[1;32m- [ STARTING Jenkins ] ------------------------------------------------------------------------------ \033[0m"
+echo -e "\033[1;32m- [ STARTING Jenkins ] ----------------------------------------------------------------------------- \033[0m"
 sudo systemctl start jenkins
 sudo systemctl daemon-reload
 
@@ -21,13 +21,13 @@ sudo systemctl daemon-reload
 sudo apt update > /dev/null 2>&1
 sudo apt install ca-certificates curl gnupg lsb-release -y > /dev/null 2>&1
 sudo mkdir -p /etc/apt/keyrings
-echo -e "\033[1;32m- [ DOWNLOADING Docker| Docker Compose | Containerd ] ----------------------------------------------- \033[0m"
+echo -e "\033[1;32m- [ DOWNLOADING Docker| Docker Compose | Containerd ] ---------------------------------------------- \033[0m"
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 sudo apt update > /dev/null 2>&1
-echo -e "\033[1;32m- [ INSTALLING Docker | Docker Compose | Containerd ] ------------------------------------------------ \033[0m"
+echo -e "\033[1;32m- [ INSTALLING Docker | Docker Compose | Containerd ] ---------------------------------------------- \033[0m"
 sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y > /dev/null 2>&1
 echo -e "\033[1;32m- [ STARTING Docker ] ------------------------------------------------------------------------------ \033[0m"
 sudo systemctl start docker > /dev/null 2>&1
@@ -35,6 +35,6 @@ sudo systemctl enable docker > /dev/null 2>&1
 sudo systemctl daemon-reload
 
 # Show default password admin jenkins
-echo -e "\033[1;33m- [ Password Default Jenkins ] ---------------------------------------------------------------------- \033[0m"
+echo -e "\033[1;33m- [ Password Default Jenkins ] --------------------------------------------------------------------- \033[0m"
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
-echo -e "\033[1;32m- [ Successfully ] ---------------------------------------------------------------------------------- \033[0m"
+echo -e "\033[1;32m- [ Successfully ] --------------------------------------------------------------------------------- \033[0m"
