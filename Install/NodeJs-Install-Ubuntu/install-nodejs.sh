aa99d9574fd616c5ffd4cf5a6a27f5de6d99139f
Vitor Reiel <52376553+vitorreiel@users.noreply.github.com>
2023-01-20 16:13:28 -0300

    att

--- Install/NodeJs-Install-Ubuntu/install-nodejs.sh
+++ Install/NodeJs-Install-Ubuntu/install-nodejs.sh
@@ -2,17 +2,17 @@
 
 sudo apt update > /dev/null 2>&1
 echo -e "\033[1;32m- [ INSTALLING Nvm ] ------------------------------------------------------------------------------ \033[0m"
-sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash > /dev/null 2>&1
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash > /dev/null 2>&1
 source ~/.bashrc
-echo -e "\033[1;32m- [ INSTALLING Npm ] ------------------------------------------------------------------------------ \033[0m"
-sudo apt install npm -y > /dev/null 2>&1
 echo -e "\033[1;33m- [ SELECT Node.js Version {Example: v14.10.0} ] -------------------------------------------------- \033[0m"
 read versaoNode
-echo -e "\033[1;32m- [ INSTALLING Node.js Version: $versaoNode ] -------------------------------------------------------- \033[0m"
-source ~/.nvm/nvm.sh
+echo -e "\033[1;32m- [ INSTALLING Node.js Version: $versaoNode ] -------------------------------------------------------- \033[>source ~/.nvm/nvm.sh
 nvm install $versaoNode
+nvm alias default $versaoNode
 nvm use $versaoNode
 node --version
+echo -e "\033[1;32m- [ INSTALLING Npm ] ------------------------------------------------------------------------------ \033[0m"
+sudo apt install npm -y > /dev/null 2>&1
 echo -e "\033[1;32m- [ Successfully Install Node.js ] ---------------------------------------------------------------- \033[0m"
 # [Show all versions Node.js]
 # nvm list-remote
