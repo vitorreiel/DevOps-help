b588d1dc9eee6a420d492874c0f067fc9b561ae9
Vitor Reiel <vitorreiel@hotmail.com>
2023-01-21 16:38:57 -0300

    att

--- Install/Docker-DockerCompose-Containerd-Ubuntu/install-docker.sh
+++ Install/Docker-DockerCompose-Containerd-Ubuntu/install-docker.sh
@@ -9,10 +9,10 @@ sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmo
 sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 sudo apt update > /dev/null 2>&1
-echo -e "\033[1;32m- [ INSTALLING Docker | Docker Compose | Containerd ] ------------------=====----------------------- \033[0m"
+echo -e "\033[1;32m- [ INSTALLING Docker | Docker Compose | Containerd ] ----------------------------------------------- \033[0m"
 sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y > /dev/null 2>&1
-echo -e "\033[1;32m- [ STARTING Docker ] ------------------------------------------------------------------------------ \033[0m"
+echo -e "\033[1;32m- [ STARTING Docker ] ------------------------------------------------------------------------------- \033[0m"
 sudo systemctl start docker
 sudo systemctl enable docker > /dev/null 2>&1
 sudo systemctl daemon-reload
-echo -e "\033[1;32m- [ Successfully ] --------------------------------------------------------------------------------- \033[0m"
+echo -e "\033[1;32m- [ Successfully ] ---------------------------------------------------------------------------------- \033[0m"
