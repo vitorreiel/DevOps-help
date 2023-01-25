038ba599a77764a7be0b5b193124e1288f121927
Vitor Reiel <vitorreiel@hotmail.com>
2023-01-25 11:21:30 -0300

    att

--- Install/Kubernetes-Install-Ubuntu/install-kubernetes.sh
+++ Install/Kubernetes-Install-Ubuntu/install-kubernetes.sh
@@ -7,11 +7,11 @@ sudo snap install kubectl --classic > /dev/null 2>&1
 echo -e "\033[1;32m- [ INSTALLING Minikube ] ------------------------------------------------------------------------------ \033[0m"
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 > /dev/null 2>&1
 sudo install minikube-linux-amd64 /usr/local/bin/minikube > /dev/null 2>&1
-sudo usermod -aG docker \$USER && newgrp docker
+sudo usermod -aG docker /$USER && newgrp docker
 minikube start --driver=docker
 echo -e "\033[1;32m- [ INSTALLING Utilitários ] --------------------------------------------------------------------------- \033[0m"
 sudo apt install unzip telnet net-tools -y > /dev/null 2>&1
-sudo echo 'alias k=kubectl' >> /etc/profile
+sudo sh -c "echo alias k=kubectl >> /etc/profile"
 echo -e "\033[1;32m- [ INSTALLING Kubectx | Kubens ] ---------------------------------------------------------------------- \033[0m"
 sudo git clone https://github.com/ahmetb/kubectx  /opt/kubectx > /dev/null 2>&1
 sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
@@ -19,6 +19,6 @@ sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
 echo -e "\033[1;32m- [ INSTALLING Bash-completion ] ----------------------------------------------------------------------- \033[0m"
 sudo apt install bash-completion -y > /dev/null 2>&1
 sudo echo 'source <(kubectl completion bash)' >> ~/.bashrc
-sudo kubectl completion bash > /etc/bash_completion.d/kubectl
+sudo sh -c 'kubectl completion bash > /etc/bash_completion.d/kubectl'
 sudo echo 'complete -F __start_kubectl k' >> ~/.bashrc
 echo -e "\033[1;32m- [ Successfully ] ------------------------------------------------------------------------------------- \033[0m"
