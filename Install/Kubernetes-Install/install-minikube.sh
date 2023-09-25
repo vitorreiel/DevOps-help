9bcd6c5d8e0be495355a2386208a72b54277e2d5
Vitor Reiel <vitorreiel@hotmail.com>
2023-09-25 20:23:18 -0300

    Update install-minikube.sh

--- Install/Kubernetes-Install/install-minikube.sh
+++ Install/Kubernetes-Install/install-minikube.sh
@@ -16,37 +16,16 @@ sudo systemctl start docker
 sudo systemctl enable docker > /dev/null 2>&1
 sudo systemctl daemon-reload
 
-echo -e "\033[1;32m- [ INSTALLING Kubernetes ] ---------------------------------------------------------------------------- \033[0m"
-sudo snap install kubectl --classic > /dev/null 2>&1
 echo -e "\033[1;32m- [ INSTALLING Minikube ] ------------------------------------------------------------------------------ \033[0m"
+sudo snap install kubectl --classic > /dev/null 2>&1
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 > /dev/null 2>&1
 sudo install minikube-linux-amd64 /usr/local/bin/minikube > /dev/null 2>&1
-sh -c 'sudo usermod -aG docker $USER && newgrp docker'
-minikube start --driver=docker > /dev/null 2>&1
-echo -e "\033[1;32m- [ INSTALLING Utilitários ] --------------------------------------------------------------------------- \033[0m"
-sudo apt install unzip telnet net-tools -y > /dev/null 2>&1
-echo -e "\033[1;32m- [ INSTALLING Kubectx | Kubens ] ---------------------------------------------------------------------- \033[0m"
-sudo git clone https://github.com/ahmetb/kubectx  /opt/kubectx > /dev/null 2>&1
-sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
-sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
-echo -e "\033[1;32m- [ INSTALLING Bash-completion ] ----------------------------------------------------------------------- \033[0m"
-sudo apt install bash-completion -y > /dev/null 2>&1
-sudo echo 'source <(kubectl completion bash)' >> ~/.bashrc
-sudo sh -c 'kubectl completion bash > /etc/bash_completion.d/kubectl'
-sudo echo 'complete -F __start_kubectl k' >> ~/.bashrc
-echo "alias k='kubectl'
-alias kc='k config view --minify | grep name'
-alias kdp='kubectl describe pod'
-alias c='clear'
-alias kd='kubectl describe pod'
-alias ke='kubectl explain'
-alias kf='kubectl create -f'
-alias kg='kubectl get pods --show-labels'
-alias kr='kubectl replace -f'
-alias ks='kubectl get namespaces'
-alias l='ls -lrt'
-alias kga='k get pod --all-namespaces'
-alias kgaa='kubectl get all --show-labels'" | tee -a ~/.bash_aliases > /dev/null 2>&1
-source ~/.bash_aliases
+minikube start
 sudo rm minikube*
 echo -e "\033[1;32m- [ Successfully ] ------------------------------------------------------------------------------------- \033[0m"
+
+# OPCIONAL
+# echo -e "\033[1;32m- [ INSTALLING Kubectx | Kubens ] ---------------------------------------------------------------------- \033[0m"
+# sudo git clone https://github.com/ahmetb/kubectx  /opt/kubectx > /dev/null 2>&1
+# sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+# sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
