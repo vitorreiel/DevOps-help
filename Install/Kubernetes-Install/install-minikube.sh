e2aaf8d25d2bf100ef38795da1a7510a786bce85
Vitor Reiel <vitorreiel@hotmail.com>
2025-01-03 14:21:42 -0300

    Update install-minikube.sh

--- Install/Kubernetes-Install/install-minikube.sh
+++ Install/Kubernetes-Install/install-minikube.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-echo -e "\033[1;32m- [ CHECKOUT ] ---------------------------------------------------------------------------- \033[0m"
+echo -e "\033[1;32m- [ CHECKOUT ] ----------------------------------------------------------------------------------------- \033[0m"
 sudo apt update > /dev/null 2>&1
 echo -e "\033[1;32m- [ INSTALLING Docker  ] ------------------------------------------------------------------------------- \033[0m"
 curl -fsSL https://get.docker.com | bash
+
+echo -e "\033[1;32m- [ INSTALLING Kubectl ] ------------------------------------------------------------------------------ \033[0m"
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
 
 echo -e "\033[1;32m- [ INSTALLING Minikube ] ------------------------------------------------------------------------------ \033[0m"
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 > /dev/null 2>&1
 sudo install minikube-linux-amd64 /usr/local/bin/minikube > /dev/null 2>&1
 sudo usermod -aG docker $USER && newgrp docker
 
-echo -e "\033[1;32m- [ EXECUTE: minikube start ] ------------------------------------------------------------------------------------- \033[0m"
+echo -e "\033[1;32m- [ EXECUTE: minikube start ] -------------------------------------------------------------------------- \033[0m"
 
 # OPCIONAL
 # echo -e "\033[1;32m- [ INSTALLING Kubectx | Kubens ] ---------------------------------------------------------------------- \033[0m"
